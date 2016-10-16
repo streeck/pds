a0a7f349a85327c05cec730a626106e2da0409f3
Charles David <raverblazer@gmail.com>
2016-10-16 21:30:25 -0200

    feat: add SingleTask Resource and patch method

--- bullet_journal/resources/tasks.py
+++ bullet_journal/resources/tasks.py
@@ -13,3 +13,12 @@ class Tasks(Resource):
     def get(self, user_email):
         user = User.query.get_or_404(user_email)
         return {'tasks': [i.serialize for i in user.tasks]}
+
+
+class SingleTask(Resource):
+    def patch(self, user_email, task_id):
+        task = Task.query.get_or_404(task_id)
+        task.done = True
+        db.session.add(task)
+        db.session.commit()
+        return task.serialize
