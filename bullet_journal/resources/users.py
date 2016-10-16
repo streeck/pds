97b6abef375bdfb737d279384b8546e6a5a4f385
Charles David <raverblazer@gmail.com>
2016-10-16 08:35:07 -0200

    fix: remove unnecessary print

--- bullet_journal/resources/users.py
+++ bullet_journal/resources/users.py
@@ -5,7 +5,6 @@ from flask_restful import Resource
 
 class Users(Resource):
     def post(self):
-        print(request)
         user = User(request.json)
         db.session.add(user)
         db.session.commit()
